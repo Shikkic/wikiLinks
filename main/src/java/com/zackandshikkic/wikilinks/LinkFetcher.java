48f266276ee50796ae8cba77e690c7bffd4bac8d
Zachary.smith <Zachary.smith@yodle.com>
2015-11-11 23:03:49 -0500

    Things be gooder now.

---
+++ main/src/java/com/zackandshikkic/wikilinks/LinkFetcher.java
@@ -0,0 +1,20 @@
+package com.zackandshikkic.wikilinks;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.List;
+
+public class LinkFetcher {
+
+    public List<String> getUrlsOnPage(String url) throws IOException {
+        Document webPage = Jsoup.connect(url).get();
+
+        Elements allLinks = webPage.select("a[href]");
+
+
+        return null;
+    }
+}
