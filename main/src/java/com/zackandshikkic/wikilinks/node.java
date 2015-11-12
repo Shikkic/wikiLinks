225a1a2eafb2090905416e5fc30e1e0e0c8238ec
Zachary.smith <Zachary.smith@yodle.com>
2015-11-11 21:49:19 -0500

    Fixed horrible project setup

--- main/src/java/com/zackandshikkic/wikilinks/node.java
+++ main/src/java/com/zackandshikkic/wikilinks/node.java
@@ -1,10 +1,9 @@
+package com.zackandshikkic.wikilinks;
+
 import java.util.Scanner;
 import java.net.*;
-import java.io.*;
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.URL;
 
 class LinkedList {
